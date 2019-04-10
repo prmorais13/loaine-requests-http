d58516598771a645a97e93c6fd98650542e8f834
prmorais13 <prmorais_13@hotmail.com>
2019-04-10 16:58:19 -0300

    Curso Angular #125: Erro Http: Alerta de Erro com Bootstrap - Incompleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
